457daf085bce6216ae05d31da5e4b0656d5c500a
nafiha <nafinalakath666@gmail.com>
2019-07-26 16:36:59 +0530

    added templates

--- numbergame/views.py
+++ numbergame/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 import random
 
-# Create your views her
+''' genrate a random and check with a user 
+entered number and return a message'''
+
 def  guesss_number(request):
 	random_number = random.randint(1,999)
 	context = {'random_number' :random_number}
 
 	if request.method == 'POST':
-		random_number = request.POST.get('random_number')
-		guess_number = request.POST.get('guess_number')
+		random_number = int(request.POST.get('random_number'))
+		guess_number = int(request.POST.get('guess_number'))
 
 		if random_number == guess_number :
 			message = "You are correct"
@@ -18,7 +20,7 @@ def  guesss_number(request):
 			message = "You are too low"
 		else:
 			message = "wrong input" 
-					
+	
 		
 		return render(request,'numbergame/result.html',{'message' :message})
 
